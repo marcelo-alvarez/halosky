28c42aad2ebb8ae7854f22723fc594b31bbf1ca3
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2021-06-18 11:53:04 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ranmap',
+      version='0.1',
+      description='Random SZ maps',
+      url='http://github.com/marcelo-alvarez/ranmap',
+      author='Marcelo Alvarez',
+      license='MIT',
+      packages=['ranmap'],
+      zip_safe=False)
